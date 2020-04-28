367262e78094c8a39f02ea6bd480ad01a9efee36
Su Anli <sual0001@gmail.com>
2020-04-28 13:43:08 +0800

    feat(home-screen): add title, timer, play button ui

--- e2e/app.test.ts
+++ e2e/app.test.ts
@@ -5,7 +5,9 @@ describe('App', () => {
     await device.reloadReactNative();
   });
 
-  it('Given any, When I open App, Then I should see "Home"', async () => {
-    await expect(element(by.text('Home'))).toBeVisible();
+  it('Given any, When I open App, Then I should see "Pomodoro", And I should see "25:00", And I should see button "Play"', async () => {
+    await expect(element(by.text('Pomodoro'))).toBeVisible();
+    await expect(element(by.text('25:00'))).toBeVisible();
+    await expect(element(by.id('pomodoroStartButton'))).toBeVisible();
   });
 });
